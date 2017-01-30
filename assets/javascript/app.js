03337576bf690d90fd30808b59ba56410d55fe7c
Asha ST <ashast@Ashas-MacBook-Pro.local>
2017-01-30 13:57:52 -0500

    adding delete logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,7 +13,6 @@
   var database = firebase.database();
   var trainsRef = database.ref("/trains"); 
 
-  var isLoggedin = false;
 
 $(document).ready(function(){
 
@@ -49,9 +48,12 @@ $(document).ready(function(){
 
 
     trainsRef.on('child_added', function(childSnapshot){
+      // $('#addTrainDiv').trigger("reset");
+      
       var trainObj = childSnapshot.val();
 
       console.log("This is a snapshot of database: " + JSON.stringify(trainObj));
+      var trainObjKey = childSnapshot.key;
 
       var first_train = trainObj.trainstart;
       var first_train_hour = first_train.split(":")[0];
@@ -59,14 +61,14 @@ $(document).ready(function(){
 
       var mmntTrainStarted = moment({hour: first_train_hour, minute: first_train_minute});
       var mmntCurrentTime = moment();
-      console.log("This is the first train converted:" + mmntTrainStarted.format());
-      console.log("Current time converted:" + mmntCurrentTime.format());
+      // console.log("This is the first train converted:" + mmntTrainStarted.format());
+      // console.log("Current time converted:" + mmntCurrentTime.format());
 
       var minutes_elapsed = Math.abs(mmntCurrentTime.diff(mmntTrainStarted,"minutes"));
-      console.log("Elapsed time.." + minutes_elapsed); 
+      // console.log("Elapsed time.." + minutes_elapsed); 
 
       var reminder_time =  minutes_elapsed % trainObj.frequency;
-      console.log("reminder.." + reminder_time);
+      // console.log("reminder.." + reminder_time);
 
       var mmntNextArrival = mmntCurrentTime.add(
               Math.abs(trainObj.frequency - reminder_time),"minutes");
@@ -74,7 +76,7 @@ $(document).ready(function(){
       var minutes_away = Math.abs(trainObj.frequency - reminder_time);
 
       var mmntOutput = moment({minute: first_train_minute})
-      console.log("Next Arrival.." + mmntNextArrival.format());
+      // console.log("Next Arrival.." + mmntNextArrival.format());
 
       if(mmntCurrentTime.diff(mmntTrainStarted,"minutes") < 0){
          mmntNextArrival = mmntTrainStarted;
@@ -83,57 +85,72 @@ $(document).ready(function(){
 
       $("#train-table").append(
         // $("tbody").append(
-        "<tr><td id='name'>" + trainObj.name + '</td>' + 
+        "<tr class='row-train-item' data-key=" + trainObjKey + ">" + 
+        "<td id='name'>" + trainObj.name + '</td>' + 
         "<td id='destination'>" + trainObj.destination + '</td>' +
         // "<td id='starttime'>" + trainObj.train_start_time + '</td>' +
         "<td id='frequency'>" + trainObj.frequency + '</td>' +
-        "<td id='nexttrain'>" + mmntNextArrival.format("h:mm a") + '</td>' +
-        "<td id='minutesaway'>" +  minutes_away + '</td></tr>');
-     
+        "<td id='nexttrain'>" + mmntNextArrival.format("h:mm a") + '</td>' +              
+        "<td id='minutesaway'>" + minutes_away + '</td>' +            
+        "<td class='trash' id='delete'><a href='#''><span class='glyphicon glyphicon-trash'></span></a>" + '</td></tr>');     
      });
 
 
-    $('#btn-login-train').on('click', function(){
+      $('#train-table').on('click', '.trash', function(){
+         var dataKey = $(this).parent().data('key');
+         console.log(dataKey);
 
-      // https://trainscheduler-ea3a8.firebaseapp.com/__/auth/handler
-      // Client ID
-      // d1a2cf47e2a32528dec3
-      //     Client Secret
-      // 6f4e00fa46ab6b8070b8b8abef446d62b9d06106
+         var removeItem = trainsRef.child(dataKey).remove();
+         console.log(removeItem);
 
-        var githubProvider = new firebase.auth.GithubAuthProvider();
-        console.log(githubProvider);
+         $(this).parent().remove();
+      })
 
-        firebase.auth().signInWithPopup(githubProvider).then(function(result) {
-        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-        var token = result.credential.accessToken;
-
-        console.log("GitHub Access Token = ", token);
-        // The signed-in user info.
-        var user = result.user;
-        console.log("GitHub Access user = ", user);
-
-        isLoggedin = true;
-        $('#btn-login-train').text("You are now logged into Github!");
-        $('#btn-login-train').prop('disabled', true);
-        $('#btn-bringup-add-train').show();
-    
-      }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        console.log(errorCode);
-
-        var errorMessage = error.message;
-        console.log(errorMessage);
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
+      trainsRef.on('child_removed', function(childSnapshot){
+          console.log("child has been removed ", childSnapshot);
       });
-    });
 
-    $('#btn-bringup-add-train').on('click', function(){
-        $('#addTrainModal').modal('show');
-    });
+
+      $('#btn-login-train').on('click', function(){
+
+        // https://trainscheduler-ea3a8.firebaseapp.com/__/auth/handler
+        // Client ID
+        // d1a2cf47e2a32528dec3
+        //     Client Secret
+        // 6f4e00fa46ab6b8070b8b8abef446d62b9d06106
+
+          var githubProvider = new firebase.auth.GithubAuthProvider();
+          console.log(githubProvider);
+
+          firebase.auth().signInWithPopup(githubProvider).then(function(result) {
+          // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+          var token = result.credential.accessToken;
+
+          // console.log("GitHub Access Token = ", token);
+          // The signed-in user info.
+          var user = result.user;
+          // console.log("GitHub Access user = ", user);
+
+          $('#btn-login-train').text("You are now logged into Github!");
+          $('#btn-login-train').prop('disabled', true);
+          $('#btn-bringup-add-train').show();
+      
+        }).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          console.log(errorCode);
+
+          var errorMessage = error.message;
+          console.log(errorMessage);
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
+      });
+
+      $('#btn-bringup-add-train').on('click', function(){
+          $('#addTrainModal').modal('show');
+      });
 });
